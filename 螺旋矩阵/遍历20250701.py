8c18490a4148c074af1f43dca43637a59074eacb
althealam <althealam0531@gmail.com>
2025-07-01 10:13:40 +0800

    commit

--- 螺旋矩阵/遍历20250701.py
+++ 螺旋矩阵/遍历20250701.py
@@ -3,20 +3,20 @@
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
         ans = []
-        m, n = len(matrix), len(matrix[0]) # m行n列
-        left, right, top, bottom = 0,n-1,0,m-1
+        m, n = len(matrix), len(matrix[0])
+        left, right, top, bottom = 0, n-1, 0, m-1
         while left<=right and top<=bottom:
-            for j in range(left, right+1): 
+            for j in range(left, right+1): # 1-2-3
                 ans.append(matrix[top][j])
-            for i in range(top+1, bottom+1):
+            for i in range(top+1, bottom+1): # 6-9
                 ans.append(matrix[i][right])
-            if top<bottom and left<right:
-                for j in range(right-1, left, -1):
+            if left<right and top<bottom:
+                for j in range(right-1, left, -1): # 8
                     ans.append(matrix[bottom][j])
-                for i in range(bottom, top, -1):
+                for i in range(bottom, top, -1): # 7-4
                     ans.append(matrix[i][left])
             left+=1
-            bottom-=1
-            top+=1
             right-=1
+            top+=1
+            bottom-=1
         return ans
